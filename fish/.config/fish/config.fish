90bb6bd2754db2c0762db374d78ac0ba34bc9f5e
naph <services+github@nstrickland.com>
2025-01-19 17:07:05 +0000

    init commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,27 @@
+fish_config theme choose "Rosé Pine"
+
+set fish_greeting
+
+starship init fish | source
+
+enable_transience
+
+set -gx fish_vi_force_cursor 1
+set -gx fish_cursor_default block
+set -gx fish_cursor_insert line blink
+set -gx fish_cursor_visual block
+set -gx fish_cursor_replace_once underscore
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+if uwsm check may-start
+	exec uwsm start hyprland.desktop
+end
+
