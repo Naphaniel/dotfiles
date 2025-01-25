7b400508edf3e7e5050c904681f7bcec86f151e4
naph <services+github@nstrickland.com>
2025-01-25 17:22:26 +0000

    nvim config

---
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "williamboman/mason.nvim",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      require("mason").setup()
+
+      require("mason-lspconfig").setup({
+        automatic_installation = true
+      })
+
+      require("lspconfig").lua_ls.setup({})
+
+    end,
+  }
+}
---
+++ nvim/.config/nvim/lua/config/plugins/rose-pine.lua
@@ -0,0 +1,14 @@
+return {
+  { 
+    "rose-pine/neovim", 
+    config = function() 
+      require("rose-pine").setup({
+        variant = "main",
+        styles = {
+          italic = false
+        }
+      })
+      vim.cmd.colorscheme("rose-pine")
+    end 
+  }
+}
