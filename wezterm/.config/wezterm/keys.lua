5e26b6b59ffa08ea3f4d456c464955ea1e339558
naph <services+github@nstrickland.com>
2025-01-27 15:01:23 +0000

    add smart splits for nvim and wezterm

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -17,34 +17,34 @@ end)
 ---@param config Config
 function M.setup(config)
   config.disable_default_key_bindings = true
-   config.keys = {
+  config.keys = {
     -- Scrollback
-    { mods = M.mod, key = "k", action = act.ScrollByPage(-0.5) },
-    { mods = M.mod, key = "j", action = act.ScrollByPage(0.5) },
+    { mods = M.mod, key = "k",     action = act.ScrollByPage(-0.5) },
+    { mods = M.mod, key = "j",     action = act.ScrollByPage(0.5) },
     -- New Tab
-    { mods = M.mod, key = "t", action = act.SpawnTab("CurrentPaneDomain") },
+    { mods = M.mod, key = "t",     action = act.SpawnTab("CurrentPaneDomain") },
     -- Splits
     { mods = M.mod, key = "Enter", action = M.smart_split },
-    { mods = M.mod, key = "|", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { mods = M.mod, key = "_", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { mods = M.mod, key = "(", action = act.DecreaseFontSize },
-    { mods = M.mod, key = ")", action = act.IncreaseFontSize },
+    { mods = M.mod, key = "|",     action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { mods = M.mod, key = "_",     action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { mods = M.mod, key = "(",     action = act.DecreaseFontSize },
+    { mods = M.mod, key = ")",     action = act.IncreaseFontSize },
     -- Move Tabs
-    { mods = M.mod, key = ">", action = act.MoveTabRelative(1) },
-    { mods = M.mod, key = "<", action = act.MoveTabRelative(-1) },
+    { mods = M.mod, key = ">",     action = act.MoveTabRelative(1) },
+    { mods = M.mod, key = "<",     action = act.MoveTabRelative(-1) },
     -- Acivate Tabs
     --
-    { mods = M.mod, key = "l", action = act({ ActivateTabRelative = 1 }) },
-    { mods = M.mod, key = "h", action = act({ ActivateTabRelative = -1 }) },
-    { mods = M.mod, key = "R", action = wezterm.action.RotatePanes("Clockwise") },
+    { mods = M.mod, key = "l",     action = act({ ActivateTabRelative = 1 }) },
+    { mods = M.mod, key = "h",     action = act({ ActivateTabRelative = -1 }) },
+    { mods = M.mod, key = "R",     action = wezterm.action.RotatePanes("Clockwise") },
     -- show the pane selection mode, but have it swap the active and selected panes
-    { mods = M.mod, key = "S", action = wezterm.action.PaneSelect({}) },
+    { mods = M.mod, key = "S",     action = wezterm.action.PaneSelect({}) },
     -- Clipboard
-    { mods = M.mod, key = "c", action = act.CopyTo("Clipboard") },
+    { mods = M.mod, key = "c",     action = act.CopyTo("Clipboard") },
     { mods = M.mod, key = "Space", action = act.QuickSelect },
-    { mods = M.mod, key = "X", action = act.ActivateCopyMode },
-    { mods = M.mod, key = "f", action = act.Search("CurrentSelectionOrEmptyString") },
-    { mods = M.mod, key = "v", action = act.PasteFrom("Clipboard") },
+    { mods = M.mod, key = "X",     action = act.ActivateCopyMode },
+    { mods = M.mod, key = "f",     action = act.Search("CurrentSelectionOrEmptyString") },
+    { mods = M.mod, key = "v",     action = act.PasteFrom("Clipboard") },
     {
       mods = M.mod,
       key = "u",
@@ -54,9 +54,9 @@ function M.setup(config)
     { mods = M.mod, key = "m", action = act.TogglePaneZoomState },
     { mods = M.mod, key = "p", action = act.ActivateCommandPalette },
     { mods = M.mod, key = "d", action = act.ShowDebugOverlay },
-    { mods = M.mod, key = "w", action = act.CloseCurrentPane { confirm = true }},
-    M.split_nav("resize", "CTRL", "LeftArrow", "Right"),
-    M.split_nav("resize", "CTRL", "RightArrow", "Left"),
+    { mods = M.mod, key = "w", action = act.CloseCurrentPane { confirm = true } },
+    M.split_nav("resize", "CTRL", "LeftArrow", "Left"),
+    M.split_nav("resize", "CTRL", "RightArrow", "Right"),
     M.split_nav("resize", "CTRL", "UpArrow", "Up"),
     M.split_nav("resize", "CTRL", "DownArrow", "Down"),
     M.split_nav("move", "CTRL", "h", "Left"),
