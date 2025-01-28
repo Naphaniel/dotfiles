3baeafeaaf7a41b671b775de13650bed904087fb
naph <services+github@nstrickland.com>
2025-01-28 21:46:49 +0000

    add mini and quickfix plugins

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,9 +1,13 @@
 return {
   {
     "echasnovski/mini.nvim",
+    lazy = false,
     config = function()
       local statusline = require("mini.statusline")
       statusline.setup({ use_icons = true })
+
+      require("mini.pairs").setup()
+      require("mini.icons").setup()
     end
   }
 }
---
+++ nvim/.config/nvim/lua/plugins/quickfix.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    opts = {},
+    config = function()
+      require("quicker").setup({
+        borders = {
+          vert = "│",
+          -- Strong headers separate results from different files
+          strong_header = "─",
+          strong_cross = "┼",
+          strong_end = "┤",
+          -- Soft headers separate results within the same file
+          soft_header = "╌",
+          soft_cross = "┼",
+          soft_end = "┤",
+        },
+        keys = {
+          {
+            ">",
+            function()
+              require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+            end,
+            desc = "Expand quickfix context",
+          },
+          {
+            "<",
+            function()
+              require("quicker").collapse()
+            end,
+            desc = "Collapse quickfix context",
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>q", function()
+        require("quicker").toggle()
+      end, {
+        desc = "Toggle quickfix",
+      })
+      vim.keymap.set("n", "<leader>l", function()
+        require("quicker").toggle({ loclist = true })
+      end, {
+        desc = "Toggle loclist",
+      })
+    end
+  }
+}
